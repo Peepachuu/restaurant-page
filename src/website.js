6e1eb0caf88de6883a6609c7309eeb7b71032076
Zafir <zafirhasan2006@gmail.com>
2022-09-17 16:48:15 +0400

    Create loading function for the body of the home page

--- src/website.js
+++ src/website.js
@@ -35,7 +35,15 @@ function createNavbarChild(name) {
 }
 
 function loadBody() {
-    
+    const content = document.querySelector("#content");
+    const main = document.createElement("section");
+    const h2 = document.createElement("h2");
+    const h4 = document.createElement("h4");
+    h2.textContent = "The Best Pizza in Town!"
+    h4.textContent = "Tasty AND Affordable."
+    main.appendChild(h2);
+    main.appendChild(h4);
+    content.appendChild(main); 
 }
 
 function loadFooter() {
