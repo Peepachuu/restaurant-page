383c387b7761f42c3878c87c9e1d28799a783e4a
Zafir <zafirhasan2006@gmail.com>
2022-10-06 13:49:12 +0400

    Make function for creating the food section in the menu page

--- src/menu.js
+++ src/menu.js
@@ -2,8 +2,12 @@ export function loadMenu() {
     const main = document.querySelector("section");
     main.textContent = "";
 
+    const menuSection = document.createElement("section");
     const infoSection = createInfoSection();
-    main.appendChild(infoSection);
+    const foodItemSection = createFoodItemSection();
+
+    menuSection.append(infoSection, foodItemSection);
+    main.appendChild(menuSection);
 }
 
 function createInfoSection() {
@@ -14,12 +18,25 @@ function createInfoSection() {
 
     const info = document.createElement("p");
     info.textContent = "Our signaure burgers are always freshly made, hand-smashed to order with 100% Certified Angus Beef that is grass fed and supports local farms. Equipped with the freshest ingredients, no two burgers are alike - from sweet-seekers, smokehouse flavor fans, heat champions, and folks just looking for a tasty classic, we've got you covered.";
-    
+
     infoSection.append(heading, info);
     return infoSection;
 }
 
-function createFoodCards(name, calories, description) {
+function createFoodItemSection() {
+    const foodItemSection = document.createElement("section");
+
+    const coloradoBurger = createFoodCard("COLORADO BURGER", "770 calories", "Certified Angus Beef, pepper jack cheese, melted cheddar cheese, grilled Anaheim chiles, lettuce, tomatoes, mayo, toasted spicy chipotle bun.");
+    const smokedBaconBrisketBurger = createFoodCard("SMOKED BACON BRISKET BURGER", "950 calories", "Certified Angus Beef, smoked aged cheddar cheese, brisket, applewood smoked bacon, pickles, bbq sauce, toasted brioche bun.");
+    const truffleMushroomBurger = createFoodCard("TRUFFLE MUSHROOM SWISS BURGER", "650 calories", "Certified Angus Beef, aged Swiss cheese, sautéed crimini mushrooms, truffle mayo, toasted bun.");
+    const barbequeBaconCheddarBurger = createFoodCard("BBQ BACON CHEDDAR BURGER", "750 calories", "Certified Angus Beef, aged cheddar cheese, applewood smoked bacon, haystack onions, bbq sauce, toasted bun.");
+    const baconSmashBurger = createFoodCard("BACON SMASH BURGER", "690 calories", "Certified Angus Beef, American cheese, applewood smoked bacon, lettuce, tomatoes, mayo, toasted bun. ");
+    const classicSmashBurger = createFoodCard("CLASSIC SMASH BURGER", "640 calories", "Certified Angus Beef, American cheese, lettuce, tomatoes, red onions, pickles, Smash Sauce®, ketchup, toasted bun.");
+
+    foodItemSection.append(coloradoBurger, smokedBaconBrisketBurger, truffleMushroomBurger, barbequeBaconCheddarBurger, classicSmashBurger, baconSmashBurger);
+    return foodItemSection;
+}
+function createFoodCard(name, calories, description) {
     const foodName = document.createElement("h3");
     foodName.textContent = name;
 
