95775fffac47c79e686c68858b406ec0874fe7b8
Zafir <zafirhasan2006@gmail.com>
2022-10-06 11:22:58 +0400

    Create bare bones info section for the menu page

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,20 @@
 export function loadMenu() {
+    const main = document.querySelector("section");
+    main.textContent = "";
+
+    const infoSection = createInfoSection();
+    main.appendChild(infoSection);
+}
+
+function createInfoSection() {
+    const infoSection = document.createElement("section");
+
+    const heading = document.createElement("h1");
+    heading.textContent = "SIGNATURE BURGERS";
+
+    const info = document.createElement("p");
+    info.textContent = "Our signaure burgers are always freshly made, hand-smashed to order with 100% Certified Angus Beef that is grass fed and supports local farms. Equipped with the freshest ingredients, no two burgers are alike - from sweet-seekers, smokehouse flavor fans, heat champions, and folks just looking for a tasty classic, we've got you covered.";
     
+    infoSection.append(heading, info);
+    return infoSection;
 }
